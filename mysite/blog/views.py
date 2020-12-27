39c317e0713919678bfff31dfccc1ac8a5c5b68f
litesh1998 <49069637+litesh1998@users.noreply.github.com>
2020-12-27 13:04:03 +0530

    added recommendation system

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 # Create your views here.
 
 
@@ -45,7 +46,13 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form=CommentForm()
 
-    return render(request, 'blog/post/detail.html', {'post': post,'comments':comments,'new_comment':new_comment, 'comment_form':comment_form})
+    post_tag_id=post.tags.values_list('id', flat=True)
+    similar_posts=Post.published.filter(tags__in=post_tag_id).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {'post': post,'comments':comments,
+                        'new_comment':new_comment, 'comment_form':comment_form,
+                        'similar_posts':similar_posts})
 
 
 def post_share(request, post_id):
