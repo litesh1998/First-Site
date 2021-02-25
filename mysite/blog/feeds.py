7ac94cfbe5479d27d70cf48066d3bbc861b17111
litesh1998 <49069637+litesh1998@users.noreply.github.com>
2021-02-25 15:26:58 +0530

    Added RSS Feed capability

---
+++ mysite/blog/feeds.py
@@ -0,0 +1,19 @@
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from django.urls import reverse_lazy
+from .models import Post
+
+
+class LatestPostFeed(Feed):
+    title = 'MyBlog'
+    link = reverse_lazy('blog:post_list')
+    description = 'News of my blog'
+
+    def items(self):
+        return Post.published.all()[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return truncatewords(item.body, 30)
